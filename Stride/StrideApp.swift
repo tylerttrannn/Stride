78847da8cc633da8be3b658db2ce9bffc23f86e3
Tyler Tran <77897545+tylerttrannn@users.noreply.github.com>
2026-01-20 17:56:29 -0800

    Initial Commit

---
+++ Stride/StrideApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrideApp.swift
+//  Stride
+//
+//  Created by Tyler Tran on 1/20/26.
+//
+
+import SwiftUI
+
+@main
+struct StrideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StrideTests/StrideTests.swift
@@ -0,0 +1,17 @@
+//
+//  StrideTests.swift
+//  StrideTests
+//
+//  Created by Tyler Tran on 1/20/26.
+//
+
+import Testing
+@testable import Stride
+
+struct StrideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
