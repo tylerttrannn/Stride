bd9fab4c97f635fe911560ea9a15622e5f991daa
Tyler Tran <77897545+tylerttrannn@users.noreply.github.com>
2026-01-22 13:56:51 -0800

    added inital view + entitlements to get access for the screen time api

---
+++ Stride/View/ScreenTimeSetupView.swift
@@ -0,0 +1,24 @@
+//
+//  ScreenTimeSetupView.swift
+//  Stride
+//
+//  Created by Tyler Tran on 1/22/26.
+//
+
+
+// todo will probably have some form of navigation later to get
+// to this view
+
+import SwiftUI
+
+struct ScreenTimeSetupView : View {
+    var body : some View {
+        VStack{
+            Text("hello")
+        }
+    }
+}
+
+#Preview{
+    ScreenTimeSetupView()
+}
