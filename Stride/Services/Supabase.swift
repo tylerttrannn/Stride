f6f41da233b4675544a134ce4c104acd5b46e7be
Michaela <mcastro01204@gmail.com>
2026-01-28 13:00:05 -0800

    Initialize supabase

---
+++ Stride/Services/Supabase.swift
@@ -0,0 +1,8 @@
+
+import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://iwmsvgrfuxtcejwjplho.supabase.co")!,
+  supabaseKey: "sb_publishable_Thdp6TvO1N8u5SuAMgzucw_o7-7pDQ8"
+)
