6d8ed7fbb0c16a1e7044310298780f7f80a61dec
Kathy Lo <klo9@uci.edu>
2026-02-01 15:37:15 -0800

    Fix TrailList to Work with Bottom Nav Bar

--- Stride/View/TrailList.swift
+++ Stride/View/TrailList.swift
@@ -11,13 +11,9 @@ struct TrailList: View {
     let screenSize: CGRect = UIScreen.main.bounds
 
     var body: some View {
-        ScrollView {
-            VStack {
                 List(trails) {
                     trail in TrailInfoCard(trail: trail)
-                }.frame(minHeight: screenSize.height*0.9).border(Color.red)
-            }
-        }
+                }
     }
 }
 
