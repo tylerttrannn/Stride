cb3ba2c365f4675c57ee34c36ea02e7730398d21
Kathy Lo <klo9@uci.edu>
2026-02-13 19:12:22 -0800

    Update TrailList to use current user location to retrieve ranked trail list

--- Stride/View/TrailList.swift
+++ Stride/View/TrailList.swift
@@ -5,14 +5,22 @@
 //  Created by Kathy Lo on 1/29/26.
 //
 import SwiftUI
+import CoreLocation
 
 struct TrailList: View {
     @State private var trails: [Trail] = []
     @State private var isLoading = true
     @State private var errorMessage: String?
     
-    @State private var latitude: CGFloat = 33.6405
-    @State private var longitude: CGFloat = -117.8443
+    @State private var latitude: CGFloat
+    @State private var longitude: CGFloat
+    
+    private let locationManager: LocationManager = LocationManager()
+    
+    init (latitude: CGFloat = 33.6405, longitude: CGFloat = -117.8443) {
+        _latitude = State(initialValue: latitude)
+        _longitude = State(initialValue: longitude)
+    }
     
     var body: some View {
         ScrollView {
@@ -35,8 +43,22 @@ struct TrailList: View {
             }
         }
         .task {
+            await getLocation()
             await loadTrails()
         }
+    }
+    
+    private func getLocation() async {
+        while locationManager.userLocation == nil {
+            try? await Task.sleep(nanoseconds: 300_000_000)
+        }
+        
+        guard let location = locationManager.userLocation else {
+            errorMessage = "Location not available"
+            isLoading = false
+            return
+        }
+        (latitude, longitude) = (location.latitude, location.longitude)
     }
     
     private func loadTrails() async {
