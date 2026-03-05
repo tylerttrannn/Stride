3d2f4a953395a0c402c2696cb46e52d521d0a93f
Kathy Lo <klo9@uci.edu>
2026-03-04 17:15:32 -0800

    Continuously update steps and stride length
    Fetch the current steps and stride length when the tabs are changed. This ensures that the displays on the app are up to date
    
    Variable name change 'stepsCount' -> 'stepCount' in TrailList. Made locationManager in trailList a StateObject. There was an issue where the location was being grabbed but the value of that instance of a LocationManager was nil, falsely reportin g that no location access was granted.

--- Stride/View/TrailList.swift
+++ Stride/View/TrailList.swift
@@ -17,20 +17,19 @@ struct TrailList: View {
     @State private var longitude: CGFloat
     @State var noticeTextHeight: CGFloat = 0
     
-    private let locationManager: LocationManager = LocationManager()
+    @StateObject private var locationManager: LocationManager = LocationManager()
     
-    private var stepsCount: Int
+    private var stepCount: Int
     private var walkingStrideLength: Double
-
     private let INCHES_PER_MILE: Double = 63360.0
     
     @EnvironmentObject var userVM: UserProfileViewModel
     
     
-    init (latitude: CGFloat = 33.6405, longitude: CGFloat = -117.8443, stepsCount: Int = 0, walkingStrideLength: Double = 25) {
+    init (latitude: CGFloat = 33.6405, longitude: CGFloat = -117.8443, stepCount: Int = 0, walkingStrideLength: Double = 25) {
         _latitude = State(initialValue: latitude)
         _longitude = State(initialValue: longitude)
-        self.stepsCount = stepsCount
+        self.stepCount = stepCount
         self.walkingStrideLength = walkingStrideLength
     }
     
@@ -40,7 +39,7 @@ struct TrailList: View {
                 if isLoading {
                     ProgressView("Finding trails...")
                         .padding()
-                } else if errorMessage == "Location not available" {
+                } else if errorMessage == "Location not available" && !trails.isEmpty {
                     Text("Notice: Current user location is not available. Displaying results based on trails best fit for you near UC Irvine.")
                         .padding(16)
                         .background(RoundedRectangle(cornerRadius: 10).fill(.yellow))
@@ -51,6 +50,10 @@ struct TrailList: View {
                             TrailInfoCard(trail: trail)
                         }
                     }
+                } else if errorMessage == "Location not available" {
+                    Text("Notice: Current user location is not available. Please fix in settings.")
+                        .padding(16)
+                        .background(RoundedRectangle(cornerRadius: 10).fill(.yellow))
                 } else if let errorMessage = errorMessage {
                     Text("Error: \(errorMessage)")
                         .foregroundColor(.red)
@@ -70,6 +73,8 @@ struct TrailList: View {
             }
         }
         .task {
+            print("Running task for getLocation and loadTrails")
+            errorMessage = nil
             await getLocation()
             await loadTrails()
         }
@@ -80,7 +85,9 @@ struct TrailList: View {
 //            try? await Task.sleep(nanoseconds: 300_000_000)
 //        }
         
+        print("\tgetLocation: \(locationManager.userLocation)")
         guard let location = locationManager.userLocation else {
+            print("getLocation: no user location available")
             errorMessage = "Location not available"
             isLoading = false
             return
@@ -93,7 +100,7 @@ struct TrailList: View {
             let results = try await TrailService().fetchRankedTrails(
                 latitude: latitude,
                 longitude: longitude,
-                remainingSteps: Double(Int(userVM.stepsGoal) - stepsCount),
+                remainingSteps: Double(Int(userVM.stepsGoal) - stepCount),
                 difficulty_pref: userVM.preferredDifficulty,
                 user_steps_per_mile: Int(INCHES_PER_MILE / walkingStrideLength)
             )
@@ -107,6 +114,6 @@ struct TrailList: View {
 }
 
 #Preview {
-    TrailList(stepsCount: 0, walkingStrideLength: 1)
+    TrailList(stepCount: 0, walkingStrideLength: 1)
         .environmentObject(UserProfileViewModel())
 }
